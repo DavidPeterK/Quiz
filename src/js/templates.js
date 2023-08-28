126696ac4f97931f2ad5d8d27e04146108e239b1
DavidPeterK <143167841+DavidPeterK@users.noreply.github.com>
2023-08-28 14:46:08 +0200

    ArrayFill

---
+++ src/js/templates.js
@@ -0,0 +1,32 @@
+let HTMLArray = [
+    {
+        'frage': ['Was ist in HTML das kleine Tag?', 'Was sind in HTML die Body-Tags?', 'Welches Tag wird in HTML verwendet, um Text fett darzustellen und als wichtigen Text zu kennzeichnen?', 'Was ist in HTML das <article>-Tag?', 'Welches Tag wird in HTML verwendet, um ein Favicon einzufügen?', 'Welcher Wert des Zielattributs öffnet in HTML einen Link in einem neuen Tab?'],
+        'antwort1': ['kleiner Text nur für IMG-Tags', 'Definiert den Header-Inhalt für das HTML-Dokument', '<em>', 'Ein Tag, der für Code verwendet wird', '<fav>', '_leer'],
+        'antwort2': ['kleiner Text nur für h1-Tags', 'definiert den Bildinhalt für das HTML-Dokument', '<b>', 'Ein Tag, der für Anführungszeichen verwendet wird', '<Link>', '_Spitze'],
+        'antwort3': ['Ein Tag, der zur Darstellung von kleinem Text für Nebenkommentare oder Copyright-Informationen verwendet wird', 'Definiert den Hauptinhalt des HTML-Dokuments', '<fett>', 'Ein Tag, der für Links verwendet wird', '<Favicon>', '_Elternteil'],
+        'antwort4': ['kleiner Text nur für div-Elemente', 'definiert den Tabelleninhalt für das HTML-Dokument', '<strong>', 'Ein Tag, der für Inhalte verwendet wird, die vom Hauptinhalt der HTML-Seite unabhängig sind', '<Symbol>', '_selbst'],
+        'result': ['3', '3', '4', '4', '2', '1']
+    }
+]
+
+let JavascriptArray = [
+    {
+        'frage': ['Welche der folgenden Methoden gibt in JavaScript die Millisekunden des angegebenen Datums gemäß der Ortszeit zurück?', 'Welches Schlüsselwort wird in JavaScript verwendet, um Teile des Codes für andere Module zugänglich zu machen?', 'Was ist in JavaScript der optionale Verkettungsoperator?', 'Was ist in JavaScript der Unterschied zwischen == und ===?', 'Was sollte in JavaScript die Ausgabe von 3 < 2 < 1 sein?', 'Was ist die Dateinamenerweiterung einer JavaScript-Datei?'],
+        'antwort1': ['getMinutes()', 'als', '?.', '== und === erlauben beide eine Typumwandlung der Werte, aber === ist strenger', 'Syntax-Fehler', '.J'],
+        'antwort2': ['getMillisec()', 'importieren', '??', '== ermöglicht eine Typumwandlung der Werte, während === dies nicht tut', 'NaN', '.javascript'],
+        'antwort3': ['getMilliseconds()', 'aus', '&&', 'Es gibt keinen Unterschied zwischen == und ===', 'FALSCH', '.js'],
+        'antwort4': ['getTheMillisecondsNow()', 'Export', '**', '== erlaubt keine Typumwandlung der Werte, während === dies tut', 'WAHR', '.py'],
+        'result': ['3', '4', '1', '2', '4', '3']
+    }
+]
+
+let CaskadingArray = [
+    {
+        'frage': ['Wofür stehen in CSS H, S und L in HSL?', 'Was bewirkt die Eigenschaft „animation-play-state“ in CSS?', 'Was ist in CSS eine at-Regel?', 'Wie heißt in CSS die relative Einheit, die auf der Schriftgröße ihres übergeordneten Elements basiert?', 'Wofür steht in CSS die VW-Einheit?', 'Was ist in CSS ein allgemeiner Haltepunktbereich, der für mobile Geräte in einer Medienabfrage verwendet wird?'],
+        'antwort1': ['Farbton, Sättigung, Blitz', 'Es bestimmt, wie lange eine Animation in Sekunden dauern soll.', 'Eine Regel, die Javascript-Funktionen vorschreibt.', 'rem', 'Breite des Ansichtsfensters', '200px - 4000px'],
+        'antwort2': ['Lebensraum, Stil, Leichtigkeit', 'Es wird verwendet, um die Animation anzuhalten, wenn sie auf „Pause“ eingestellt ist.', 'Eine Regel, die nur für den Stil von <p>-Elementen gilt.', 'vw', 'Ansicht Breite Höhe', '320px - 480px'],
+        'antwort3': ['Farbton, Sättigung, Helligkeit', 'Es wird verwendet, um das Element nach Ende der Animation zu formatieren.', 'Eine Regel, die vorschreibt, wie der HTML-Code formatiert wird.', 'vh', 'Höhe des Ansichtsfensters', '100px - 150px'],
+        'antwort4': ['Farbton, Stil, Leichtigkeit', 'Es gibt die Richtung für die Animation vor.', 'Eine Regel, die vorschreibt, wie das CSS basierend auf bestimmten Bedingungen aussehen wird.', 'em', 'Gewicht des Ansichtsfensters', '1000px - 5000px'],
+        'result': ['3', '2', '4', '4', '1', '2']
+    }
+]
